b93e537a3f5e086e32fb494018cfd476ada7d763
Daniel <dgallups@purdue.edu>
2023-12-05 15:55:27 -0500

    beautiful, p1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,12 +1,12 @@
-use std::collections::HashMap;
-
 fn main() {
     let input = include_str!("./input.txt");
 
     let lines = input
         .split('\n')
         .map(|l| l.trim().to_string())
-        .collect::<Vec<String>>();
+        .collect::<Vec<String>>()
+        .into_iter()
+        .peekable();
 
     let mut lines = lines.into_iter();
 
@@ -15,52 +15,52 @@ fn main() {
         .unwrap()
         .split(' ')
         .skip(1)
-        .map(|v| v.parse::<u32>().unwrap())
-        .collect::<Vec<u32>>();
+        .map(|v| v.parse::<u128>().unwrap())
+        .collect::<Vec<u128>>();
 
     let mut seeds = og_seeds.clone();
 
     println!("{:?}", seeds);
 
     //lines.for_each(|v| println!("{}", v));
-
     loop {
-        println!("in loop");
         let line_clone = &mut lines;
 
-        let mut map: HashMap<u32, u32> = HashMap::new();
+        let mut mapped = (0..seeds.len()).map(|_| false).collect::<Vec<bool>>();
 
         line_clone
+            .peekable()
             .skip_while(|line| !line.contains("map"))
             .skip(1)
             .take_while(|line| !line.is_empty())
             .for_each(|map_line| {
                 let vals = map_line
                     .split(' ')
-                    .map(|v| v.parse::<u32>().unwrap())
-                    .collect::<Vec<u32>>();
+                    .map(|v| v.parse::<u128>().unwrap())
+                    .collect::<Vec<u128>>();
                 let destination = vals[0];
                 let source = vals[1];
                 let len = vals[2];
 
-                for i in 0..len {
-                    map.insert(source + i, destination + i);
-                }
-            });
-        println!("here 1");
+                seeds.iter_mut().enumerate().for_each(|(i, seed)| {
+                    if mapped[i] {
+                        return;
+                    }
 
-        seeds.iter_mut().for_each(|seed| {
-            if let Some(val) = map.get(seed) {
-                *seed = *val;
-            }
-        });
+                    if (source..source + len).contains(seed) {
+                        *seed = destination + (*seed - source);
+                        mapped[i] = true;
+                    }
+                });
+            });
+
         println!("{:?}", seeds);
-        if map.is_empty() {
+        if line_clone.peek().is_none() {
             break;
         }
     }
 
-    let res = seeds.into_iter().fold(u32::MAX, |acc, val| acc.min(val));
+    let res = seeds.into_iter().fold(u128::MAX, |acc, val| acc.min(val));
 
     println!("result: {}", res);
 }
